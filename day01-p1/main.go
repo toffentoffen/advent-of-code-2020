cba5d0d0b60936da54bc0cfa1a1cc6f4e1ab363b
Marc Navarro <marc.navarro@teamwork.com>
2020-12-02 18:16:13 +0100

    Add day1 solutions

---
+++ day01-p1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	d, err := readInput("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, x := range d.ints {
+		for _, y := range d.ints {
+			if x + y == 2020 {
+				fmt.Printf("result: %d\n", x * y)
+				return
+			}
+		}
+	}
+}
+
+type data struct{
+	ints []int
+}
+
+func readInput(path string) (*data, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %s: %v", path, err)
+	}
+	defer f.Close()
+
+	var d data
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		var n int
+		_, err := fmt.Sscanf(s.Text(), "%d", &n)
+		if err != nil {
+			log.Fatalf("could not read %s: %v", s.Text(), err)
+		}
+		d.ints = append(d.ints, n)
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return &d, s.Err()
+}
